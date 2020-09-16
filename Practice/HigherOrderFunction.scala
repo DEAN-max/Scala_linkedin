588765e6e1f648153699239d6e7d1a82230bcc0d
DEAN-max <58270622+DEAN-max@users.noreply.github.com>
2020-09-16 11:18:06 +0530

    Update HigherOrderFunction.scala

--- Practice/HigherOrderFunction.scala
+++ Practice/HigherOrderFunction.scala
@@ -1,9 +1,9 @@
 // Function variable.
-val double = (i: Int) => i * 2
+val double = (i: Int) => i * 2                  // This function will just multiply by 2. 
 // double: Int => Int = $$Lambda$875/0x000000084056d840@585513a8
 
-def higherOrder(x: Int, y: Int => Int) = y(x)
+def higherOrder(x: Int, y: Int => Int) = y(x)   // First parameter is an integer number and the second one is a function that takes an int and returns an integer value as well.
 // higherOrder: (x: Int, y: Int => Int)Int
 
-higherOrder(5, double)
+higherOrder(5, double)                          // We are passing the value and the function name as both arguments.
 // res3: Int = 10
