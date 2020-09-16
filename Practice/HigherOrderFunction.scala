abce1705772b898a8a346ac0ab9233cff4a84ac0
DEAN-max <58270622+DEAN-max@users.noreply.github.com>
2020-09-16 11:19:36 +0530

    Update HigherOrderFunction.scala

--- Practice/HigherOrderFunction.scala
+++ Practice/HigherOrderFunction.scala
@@ -3,6 +3,7 @@ val double = (i: Int) => i * 2                  // This function will just multi
 // double: Int => Int = $$Lambda$875/0x000000084056d840@585513a8
 
 def higherOrder(x: Int, y: Int => Int) = y(x)   // First parameter is an integer number and the second one is a function that takes an int and returns an integer value as well.
+// Inside the function body the function is called by passing the value to it.
 // higherOrder: (x: Int, y: Int => Int)Int
 
 higherOrder(5, double)                          // We are passing the value and the function name as both arguments.
